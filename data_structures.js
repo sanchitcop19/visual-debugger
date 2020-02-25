c8c6a2b7f9d8e7d4ace05d69c8d4961aaf9e7fa9
Sanchit <sanchitcop19@Gmail.com>
2020-02-25 04:35:28 -0500

    Began work on insert

--- data_structures.js
+++ data_structures.js
@@ -1,31 +1,60 @@
-class VisualArray extends Array{
-
-    constructor(){
+class VisualArray extends Array {
+    constructor() {
         super();
     }
 
     capacity = 1;
 
-    get capacity(){
+    get capacity() {
         return this.capacity;
     }
 
-    doubleCapacity(){
-        this.capacity = 2*this.capacity;
+    doubleCapacity() {
+        this.capacity = 2 * this.capacity;
     }
 
-    push(num){
-        
-        this.update();
-        if (this.length >= this.capacity){
+    push(num) {
+        if (this.length >= this.capacity) {
             this.doubleCapacity();
         }
-        console.log(this.length); 
-        return super.push(num);
+        console.log(this.length);
+        const result = super.push(num);
+        this.update();
+        return result;
     }
 
-    update(){
+    insert(index, value) {
+        const result = super.insert(index, value);
+        this.update();
+        return result;
+    }
+
+    update() {
         // Updates display in the DOM
+        const arrayDiv = document.getElementById("array");
+        const previousList = document.getElementById("list-id");
+        if (previousList) {
+            arrayDiv.removeChild(previousList);
+        }
+
+        const arrayUl = document.createElement("ul");
+        arrayUl.id = "list-id";
+        arrayUl.className = "list";
+        arrayDiv.appendChild(arrayUl);
+
+        let arrayLi;
+
+        for (let i = 0; i < array.capacity; ++i) {
+            arrayLi = document.createElement("li");
+            if (i < array.length) {
+                arrayLi.innerHTML = array[i];
+            } else {
+                arrayLi.innerHTML = "ø";
+            }
+            arrayLi.id = "list-item-" + i;
+            arrayLi.className = "list-item";
+            arrayUl.appendChild(arrayLi);
+        }
         return;
     }
 }
--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-const arrayDiv = document.getElementById("array")
-const arrayUl =  document.createElement("ul");
-arrayUl.className = "list";
-arrayDiv.appendChild(arrayUl);
+
 
 const array = new VisualArray();
 array.push(1);
 array.push(2);
 array.push(3);
-array.push(4);
-array.push(5);
+array.push(3);
+array.push(3);
 
-let arrayLi;
 
-for (let i = 0; i < array.capacity; ++i) {
-    arrayLi = document.createElement("li");
-    if (i < array.length){
-        arrayLi.innerHTML = array[i];    
-    } else{
-        arrayLi.innerHTML = "ø"; 
-    }
-    arrayLi.id = "list-item-" + i;
-    arrayLi.className = "list-item";
-    arrayUl.appendChild(arrayLi);
-};
 
+const insertButton = document.getElementById("insert");
+insertButton.addEventListener("click", (e) => {
+    console.log(e);
+    const index = document.getElementById('index');
+    const value = document.getElementById('value');
+    array.insert(index, value);
+})
